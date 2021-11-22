92435721a5e42dce1c7154b4b8eaea8a79015ced
Your Name <you@example.com>
2021-11-22 17:42:09 +0100

    carga inicial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,57 @@
+/* eslint-disable vue/require-v-for-key */
 <template>
-  <div class="hello">
+
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    <h1 >{{texto}}</h1>
+    <h1>{{5*6}}</h1>
+    <button v-on:click="cambiarTexto('texto cambiado desde el evento')">pulsa para cambiar</button>
+    <input type="text"  v-model="texto" >
+
+    <button v-on:click="agregarPersona()">agregar persona</button>
+
+    
+    <ul v-for="(persona,index) in personas" :key="index" >
+      <li v-if="index % 2==0">{{index}}-{{persona.nombre}}-{{persona.apellido}}-{{persona.telefono}}</li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    
+
 </template>
 
 <script>
+class Persona{
+  constructor(nombre, apellido, telefono){
+    this.nombre=nombre;
+    this.apellido=apellido;
+    this.telefono=telefono;
+  }
+}
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      texto:"texto inicial",
+      personas:[]
+    }
+  },
+  methods:{
+    cambiarTexto(textoNuevo){
+       this.texto=textoNuevo;
+    },
+    dameValor(){
+      return "valor"
+    },
+    cambiarDesdeInput(valor){
+      this.texto=valor;
+    },
+    agregarPersona(){
+
+     let  persona=new Persona("federico","garcia","9393939");
+      this.personas.push(persona);
+    }
   }
 }
 </script>
