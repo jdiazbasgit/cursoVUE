76f8ca63185f831556f8a8aed4b3f3dbbe39e329
Your Name <you@example.com>
2021-11-23 18:13:52 +0100

    composicion desde javascript

--- dia2/src/components/HelloWorld.vue
+++ dia2/src/components/HelloWorld.vue
@@ -1,67 +1,74 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1 style="color:red">{{texto}}</h1>
-    <ComponenteBoton :cambiar="cambiarTextoDeTexto"/>
+    <h1 style="color: red">{{ texto }}</h1>
+    <ComponenteBoton :cambiar="cambiarTextoDeTexto" />
+    <ul id="ul"></ul>
     <ul v-for="pais, index in paises" :key="index" id="ul">
-      <li>{{pais.name.common}}</li>
+    <li>{{ pais.name.common }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-
-import ComponenteBoton from './ComponenteBoton.vue'
-import axios from 'axios'
+import ComponenteBoton from "./ComponenteBoton.vue";
+import axios from "axios";
 export default {
   components: { ComponenteBoton },
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   beforeCreate() {
-    console.log("antes del componente")
+    console.log("antes del componente");
   },
-  created(){
-    console.log("componente creado")
-  axios.get("https://restcountries.com/v3.1/region/europe").then((response)=>{
-    this.paises=response.data;
-    let li=document.createElement("li");
-  let ul= document.querySelector("#ul");
-  ul.appendChild(li)
-  }).catch(function(error){
-    console.log(error);
-  })
+  created() {
+    console.log("componente creado");
+    let ul1 = document.querySelector("#ul");
+    console.log(ul1)
+    axios
+      .get("https://restcountries.com/v3.1/region/europe")
+      .then((response) => {
+        this.paises = response.data;
+       /* let ul = document.querySelector("#ul");
+        this.paises.forEach((element) => {
+          let li = document.createElement("li");
+          li.innerHTML = element.name.common;
+          ul.appendChild(li);
+        });*/
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   },
-  mounted(){
-    console.log("creado arbol dom")
+  mounted() {
+    let ul1 = document.querySelector("#ul");
+    console.log(ul1);
+    
   },
-  updated(){
-    console.log("modificacion de componente")
+  updated() {
+    console.log("modificacion de componente");
   },
   unmounted() {
-    console.log("componente destruido")
+    console.log("componente destruido");
   },
-  data()
-  {
-    return{
-      texto:"estamos en un curso",
-      paises:null
-    }
+  data() {
+    return {
+      texto: "estamos en un curso",
+      paises: null,
+    };
   },
-  methods:{
-    async cambiarTextoDeTexto(nuevoTexto){
-      this.texto= nuevoTexto;
-    }
-
+  methods: {
+    async cambiarTextoDeTexto(nuevoTexto) {
+      this.texto = nuevoTexto;
+    },
   },
-  watch:{  
-    texto:function(newValue,oldValue){
-      console.log(`hemos cambiado  el "valor" ${oldValue} por ${newValue}`)
-
-    }
-  }
-}
+  watch: {
+    texto: function (newValue, oldValue) {
+      console.log(`hemos cambiado  el "valor" ${oldValue} por ${newValue}`);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
